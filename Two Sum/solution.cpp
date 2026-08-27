adde559e86f8c2bb084c89cf77313fe24adf1cc1
Yogesh Bohara <yogeshbohara254@gmail.com>
2026-08-27 22:31:19 +0545

    Update Two Sum Solution

--- Two Sum/solution.cpp
+++ Two Sum/solution.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
 using namespace std;
 
-int main(){
-    
-}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            int complement = target - nums[i];
+
+            if (map.find(complement) != map.end()) {
+                return {map[complement], i};
+            }
+
+            map[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
