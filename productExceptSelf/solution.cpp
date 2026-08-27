5a76eec956b5701856f5711ea2a22cb54690d3be
Yogesh Bohara <yogeshbohara254@gmail.com>
2026-08-27 23:35:15 +0545

    Add productExceptSelf Soultion

---
+++ productExceptSelf/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        vector<int> result(nums.size(), 1);
+
+        int prefix = 1;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            result[i] = prefix;
+
+            prefix = prefix * nums[i];
+        }
+
+        int suffix = 1;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+
+            result[i] = result[i] * suffix;
+
+            suffix = suffix * nums[i];
+        }
+
+        return result;
+    }
+};
